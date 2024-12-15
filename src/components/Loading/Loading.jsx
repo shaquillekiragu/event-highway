135caf10d56088fd33fef77f3e5a4c281110f22a
shaquillekiragu <skiragu77@gmail.com>
2024-12-15 21:19:14 +0000

    EventPage.jsx

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,3 @@
+function Loading() {
+  return <div className="loader"></div>;
+}
--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,7 +1,39 @@
 import { useState, useEffect } from "react";
+import getEvents from "../../api";
+import EventCard from "../components/EventCard/EventCard.jsx";
+import Loading from "../components/Loading/Loading.jsx";
 
 function EventsPage() {
   const [eventsList, setEventsList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        const response = await getEvents();
+        setEventsList(response);
+        setIsLoading(false);
+      } catch (err) {
+        console.error(err, " << error");
+      }
+    }
+    fetchEvents();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+  return (
+    <ul>
+      {eventsList.map((event) => {
+        return (
+          <li>
+            <EventCard event={event} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default EventsPage;
