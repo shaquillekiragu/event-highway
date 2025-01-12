c01a5b1a6ae0b6f989589caf745ed76749f365a9
shaquillekiragu <skiragu77@gmail.com>
2025-01-12 20:49:30 +0000

    Initial WelcomePage.jsx setup

--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -1,5 +1,38 @@
+import { useNavigate } from "react-router-dom";
 import "../stylesheets/WelcomePage.css";
 
-function WelcomePage() {}
+function WelcomePage() {
+  const navigate = useNavigate();
+
+  function handleLoginClick() {
+    navigate("/login");
+  }
+  function handleSignupClick() {
+    navigate("/signup");
+  }
+
+  return (
+    <>
+      <p>Welcome to Event Highway!</p>
+      <button onClick={handleLoginClick} className="logButton">
+        Login
+      </button>
+      <button onClick={handleSignupClick} className="logButton">
+        Sign Up!
+      </button>
+      <br />
+      <br />
+      <label htmlFor="proceed">Proceed without logging in: </label>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+        className="logButton"
+      >
+        Proceed
+      </button>
+    </>
+  );
+}
 
 export default WelcomePage;
