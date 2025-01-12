8875a7263287cb978169af472cd92d67cb0df742
shaquillekiragu <skiragu77@gmail.com>
2025-01-12 20:46:59 +0000

    Initial SignUpPage.jsx setup

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/UserContext";
+import { postUser } from "../api";
+// import SignUpForm from "../components/SignUpForm/SignUpForm";
 import "../stylesheets/SignUpPage.css";
 
-function SignUpPage() {}
+function SignUpPage() {
+  const [hasPosted, setHasPosted] = useState(true);
+  const [hasInputtedInfo, setHasInputtedInfo] = useState(false);
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [adminStatus, setAdminStatus] = useState(null);
+
+  const { authUser, setAuthUser, isLoggedIn, setIsLoggedIn } = useAuth();
+
+  const navigate = useNavigate();
+}
 
 export default SignUpPage;
