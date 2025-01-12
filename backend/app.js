08407a3e29f5b0bf32c8ea99e2021e2e691829e1
shaquillekiragu <skiragu77@gmail.com>
2025-01-12 15:58:02 +0000

    Integrating cors functionality and 'returning' error handling responses

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 const healthcheck = require("./controllers/healthcheck.controller");
 const getApi = require("./controllers/api.controller");
 
@@ -41,7 +50,7 @@ app.get("/api/users/:userId", getUser);
 app.patch("/api/users/:userId", patchUser);
 
 app.all("*", (request, response) => {
-  response.status(404).send({ msg: "Endpoint not found" });
+  return response.status(404).send({ msg: "Endpoint not found" });
 });
 
 // ERROR HANDLING:
@@ -51,7 +60,7 @@ app.use((err, request, response, next) => {
   //   console.log(err.status, " < error status 1");
   //   console.log(err.msg, " < error msg 1");
   if (err.status && err.msg) {
-    response.status(err.status).send({ msg: err.msg });
+    return response.status(err.status).send({ msg: err.msg });
   }
   next(err);
 });
@@ -60,15 +69,15 @@ app.use((err, request, response, next) => {
   //   console.log(err, " < error 2");
   //   console.log(err.code, " < error code 2");
   if (err.code === "22P02" || err.code === "23502") {
-    response.status(400).send({ msg: "Bad Request" });
+    return response.status(400).send({ msg: "Bad Request" });
   } else if (err.code === "23503") {
-    response.status(404).send({ msg: "Not Found" });
+    return response.status(404).send({ msg: "Not Found" });
   }
   next(err);
 });
 
 app.use((err, request, response) => {
-  response.status(500).send({ message: "Internal server error" });
+  return response.status(500).send({ message: "Internal server error" });
 });
 
 module.exports = app;
