e8879ac8fa5a582222c676f5066019ad2f89e4c5
shaquillekiragu <skiragu77@gmail.com>
2025-01-12 20:48:57 +0000

    EventsPage.jsx page navigation functionality

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import getEvents from "../api.js";
 import EventCard from "../components/EventCard/EventCard.jsx";
 import Loading from "../components/Loading/Loading.jsx";
@@ -8,19 +9,27 @@ function EventsPage() {
   const [eventsList, setEventsList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  const navigate = useNavigate();
+
+  function handleProfileClick() {
+    navigate("/profile/:user_id");
+  }
+
   useEffect(() => {
     async function fetchEvents() {
       try {
         const response = await getEvents();
         setEventsList(response.data.events);
-        console.log(eventsList, " <<< eventsList");
+        console.log(response.data.events, " <<< response.data.events");
         setIsLoading(false);
       } catch (err) {
-        console.error(err, " << error");
+        console.error(err, " << fetchEvents error");
       }
     }
     fetchEvents();
   }, []);
+
+  console.log(eventsList, " <<< eventsList");
 
   if (isLoading) {
     return <Loading />;
@@ -28,10 +37,13 @@ function EventsPage() {
   return (
     <>
       <h1>Events</h1>
+      <button onClick={handleProfileClick}>Profile Page</button>
       <ul>
-        {eventsList.map((event) => {
-          return <li key={event.event_id}>{<EventCard event={event} />}</li>;
-        })}
+        {eventsList.map((event) => (
+          <li key={event.event_id}>
+            <EventCard event={event} />
+          </li>
+        ))}
       </ul>
     </>
   );
