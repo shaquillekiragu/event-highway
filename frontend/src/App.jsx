3720f26d30ca9869d383252cd9d05d90a22c9d81
shaquillekiragu <skiragu77@gmail.com>
2025-01-12 20:44:50 +0000

    Adding additional page routes to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import UserProvider from "./contexts/UserContext.jsx";
 import "./App.css";
-import WelcomePage from "./pages/WelcomePage";
-import EventsPage from "./pages/EventsPage";
-import ViewEvent from "./pages/ViewEvent";
+import WelcomePage from "./pages/WelcomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import EventsPage from "./pages/EventsPage.jsx";
+import ViewEvent from "./pages/ViewEvent.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
 
 function App() {
   return (
     <UserProvider>
       <Routes>
         <Route path="/" element={<WelcomePage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
         <Route path="/home" element={<EventsPage />}></Route>
         <Route path="/viewEvent/:event_id" element={<ViewEvent />}></Route>
+        <Route path="/profile/:user_id" element={<ProfilePage />}></Route>
       </Routes>
     </UserProvider>
   );
