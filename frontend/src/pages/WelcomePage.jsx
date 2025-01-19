580ddb574b97d591fed1050da02b1880e6098246
shaquillekiragu <skiragu77@gmail.com>
2025-01-19 23:04:55 +0000

    Welcome page fully styled

--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -14,15 +14,19 @@ function WelcomePage() {
   return (
     <section className="welcomePage">
       <div className="thinBlackBanner"></div>
-      <div className="welcomeContainer">
-        <article className="welcomeSubcontainer">
+      <article className="welcomeContainer">
+        <div className="welcomeLayerOne">
           <p>Welcome to Event Highway!</p>
+        </div>
+        <div className="welcomeLayerTwo">
           <button onClick={handleLoginClick} className="logButton">
             Login
           </button>
           <button onClick={handleSignupClick} className="logButton">
             Sign Up!
           </button>
+        </div>
+        <div className="welcomeLayerThree">
           <br />
           <br />
           <label htmlFor="proceed">Proceed without logging in: </label>
@@ -34,8 +38,8 @@ function WelcomePage() {
           >
             Proceed
           </button>
-        </article>
-      </div>
+        </div>
+      </article>
     </section>
   );
 }
