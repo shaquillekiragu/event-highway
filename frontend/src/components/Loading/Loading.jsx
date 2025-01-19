daa8b7e9186a6f93d9b76da8f5d518662f4753c4
shaquillekiragu <skiragu77@gmail.com>
2025-01-19 21:54:27 +0000

    Styling and switching html elements in loading component

--- frontend/src/components/Loading/Loading.jsx
+++ frontend/src/components/Loading/Loading.jsx
@@ -2,12 +2,12 @@ import "./Loading.css";
 
 function Loading({ page }) {
   return (
-    <>
+    <div className="loadingComponent">
       <div className="loader"></div>
       <p>
         <em>{page} page loading...</em>
       </p>
-    </>
+    </div>
   );
 }
 
