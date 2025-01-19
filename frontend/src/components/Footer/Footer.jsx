d47bb3025eb2929db3e18fd1146386738a852688
shaquillekiragu <skiragu77@gmail.com>
2025-01-19 21:53:13 +0000

    Creating and styling a footer component

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>@ 2024 Event Highway | Copyright 2024</p>
+      <p></p>
+    </footer>
+  );
+}
+
+export default Footer;
