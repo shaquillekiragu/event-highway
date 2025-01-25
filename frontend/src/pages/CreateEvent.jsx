c80b1cf1248f9c11bcc86449a5130c630cf43b41
Shaquille Kiragu <shaquille@toru.digital>
2025-01-25 19:28:24 +0000

    CreateEvent redone

--- frontend/src/pages/CreateEvent.jsx
+++ frontend/src/pages/CreateEvent.jsx
@@ -1,5 +1,17 @@
 import "../stylesheets/CreateEvent.css";
+import { useState, useEffect } from "react";
+import { postEvent } from "../api";
+import Loading from "../components/Loading/Loading";
 
-function CreateEvent() {}
+function CreateEvent() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [,] = useState(true);
+
+  useEffect(() => {}, []);
+
+  if (isLoading) {
+    <Loading page="" />;
+  }
+}
 
 export default CreateEvent;
