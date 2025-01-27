df32abbc2e397e5e798dcd4c4f34438d64837173
shaquillekiragu <skiragu77@gmail.com>
2025-01-27 23:27:44 +0000

    Creating the NotAnAdmin component

---
+++ frontend/src/components/NotAnAdmin/NotAnAdmin.jsx
@@ -0,0 +1,11 @@
+import "./NotAnAdmin.css";
+
+function NotAnAdmin() {
+  return (
+    <main className="notAnAdmin">
+      <h1>Sign in as an event administrator to access this page...</h1>
+    </main>
+  );
+}
+
+export default NotAnAdmin;
