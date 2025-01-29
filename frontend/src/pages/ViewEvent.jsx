d7c96a989c5b566157f5e43c2313602a448b0df1
shaquillekiragu <skiragu77@gmail.com>
2025-01-29 00:33:19 +0000

    Initial ViewEvent page structuring

--- frontend/src/pages/ViewEvent.jsx
+++ frontend/src/pages/ViewEvent.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { getEvent } from "../api";
+import { useParams, useNavigate } from "react-router-dom";
+import { getEvent, deleteEvent } from "../api";
+import { useAuth } from "../contexts/UserContext.jsx";
 import FormatDatetimeFrontend from "../components/FormatDatetime/FormatDatetimeFrontend";
 import Loading from "../components/Loading/Loading.jsx";
 import "../stylesheets/ViewEvent.css";
@@ -9,6 +10,9 @@ function ViewEvent() {
   const { event_id } = useParams();
   const [event, setEvent] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+
+  const { authUser } = useAuth();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchEventView() {
@@ -26,43 +30,59 @@ function ViewEvent() {
 
   console.log(event, " <<< event");
 
+  function handleUpdateClick() {
+    navigate(`/update-event/${event.event_id}`);
+  }
+
+  function handleDeleteClick() {}
+
   if (isLoading) {
     return <Loading page={"View Event"} />;
   }
   return (
-    <main>
-      <h2>{event.event_name}</h2>
-      <p>
-        Date posted:{" "}
-        {<FormatDatetimeFrontend sqlTimestamp={event.created_at} />}
-      </p>
-      <p>Publisher: {event.publisher}</p>
-      <p>Category: {event.category}</p>
-      <p>
-        Event Start:{" "}
-        {<FormatDatetimeFrontend sqlTimestamp={event.event_start} />}
-      </p>
-      <p>
-        Event Finish:{" "}
-        {<FormatDatetimeFrontend sqlTimestamp={event.event_end} />}
-      </p>
-      <p>Host: {event.host}</p>
-      <p>Description: {event.event_description}</p>
-      <p>
-        {event.is_online
-          ? "Venue: This event is online"
-          : `Venue: The venue for this event is ${event.venue} at ${event.venue_address}`}
-      </p>
-      <p>
-        {event.is_free
-          ? "Price: This event is free"
-          : `Price: The price for this event is ${event.cost_in_gbp}`}
-      </p>
-      <p>
-        {event.is_limit
-          ? `Attendee Limit: The attendee limit for this event is ${event.attendee_limit}`
-          : "Attendee Limit: There is no attendee limit for this event"}
-      </p>
+    <main className="viewEventContainer">
+      <section className="viewLayerOne">
+        <h2>{event.event_name}</h2>
+        <p>
+          Date posted:{" "}
+          {<FormatDatetimeFrontend sqlTimestamp={event.created_at} />}
+        </p>
+        <p>Publisher: {event.publisher}</p>
+        <p>Category: {event.category}</p>
+        <p>
+          Event Start:{" "}
+          {<FormatDatetimeFrontend sqlTimestamp={event.event_start} />}
+        </p>
+        <p>
+          Event Finish:{" "}
+          {<FormatDatetimeFrontend sqlTimestamp={event.event_end} />}
+        </p>
+        <p>Host: {event.host}</p>
+        <p>Description: {event.event_description}</p>
+        <p>
+          {event.is_online
+            ? "Venue: This event is online"
+            : `Venue: The venue for this event is: ${event.venue}, ${event.venue_address}`}
+        </p>
+        <p>
+          {event.is_free
+            ? "Price: This event is free"
+            : `Price: The price for this event is: ${event.cost_in_gbp}`}
+        </p>
+        <p>
+          {event.is_limit
+            ? `Attendee Limit: The attendee limit for this event is: ${event.attendee_limit}`
+            : "Attendee Limit: There is no attendee limit for this event"}
+        </p>
+      </section>
+      {authUser.isadmin ? (
+        <section className="viewLayerTwo">
+          <button onClick={handleUpdateClick}></button>
+          <button onClick={handleDeleteClick}></button>
+        </section>
+      ) : (
+        <></>
+      )}
     </main>
   );
 }
