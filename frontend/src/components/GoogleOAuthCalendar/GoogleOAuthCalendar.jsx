455b8884024a105c82d8051d73aec4b352d1134c
Shaquille Kiragu <shaquille@toru.digital>
2025-01-30 17:32:05 +0000

    Setting up google oauth calendar component

---
+++ frontend/src/components/GoogleOAuthCalendar/GoogleOAuthCalendar.jsx
@@ -0,0 +1,19 @@
+import ApiCalendar from "react-google-calendar-api";
+
+function GoogleOAuthCalendar() {
+  const config = {
+    clientId:
+      "891301921688-7ms3rct4cfe9h22hfolm3pcl9ona71un.apps.googleusercontent.com",
+    apiKey: "AIzaSyDC3gJJAJcY22P3M0Oc26KIwEcbMse5sUo",
+    scope: "https://www.googleapis.com/auth/calendar",
+    discoveryDocs: [
+      "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+    ],
+  };
+
+  const apiCalendar = new ApiCalendar(config);
+
+  return <></>;
+}
+
+export default GoogleOAuthCalendar;
