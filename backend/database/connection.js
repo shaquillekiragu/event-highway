98b8d441a4fc253825b397c9a675174478f84091
shaquillekiragu <skiragu77@gmail.com>
2025-03-03 00:07:11 +0000

    Making changes to backend for hosting the database virtually

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,13 +1,22 @@
 const { Pool } = require("pg");
 
+const ENV = process.env.NODE_ENV || "development";
+
 require("dotenv").config({
   path: process.env.NODE_ENV === "test" ? ".env.test" : ".env.development",
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 console.log("PGDATABASE:", process.env.PGDATABASE);
+
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
 
 module.exports = new Pool();
