8d30f03b59d17a659d83798b9c5ff6a7ab3a5fef
shaquillekiragu <skiragu77@gmail.com>
2025-03-07 00:38:30 +0000

    Changes made to connection  file

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-const ENV = process.env.NODE_ENV || "development";
+const ENV = process.env.NODE_ENV || "production";
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
@@ -8,16 +8,16 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
+console.log(">>> ENV:", ENV);
 console.log(">>> PGDATABASE:", process.env.PGDATABASE);
+console.log(">>> DATABASE_URL:", process.env.DATABASE_URL ? "Set" : "Not Set");
 
-const config =
-  ENV === "production"
-    ? { connectionString: process.env.DATABASE_URL, max: 2 }
-    : {};
+const config = {};
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+  config.ssl = { rejectUnauthorized: false };
 }
 
 module.exports = new Pool(config);
