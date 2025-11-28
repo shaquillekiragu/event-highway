18d622e479aa80bc60d17aa6f58560fda0696801
Shaquille Kiragu <shaquille@toru.digital>
2025-11-28 00:18:20 +0000

    increase max number of db connections for production

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -22,7 +22,7 @@ const config = {};
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
+  config.max = 15;
   config.ssl = { rejectUnauthorized: false };
   config.family = 4;
 }
