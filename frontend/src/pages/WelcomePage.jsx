32912aaefbb7615108209c2614659fd9bd44cd41
Shaquille Kiragu <shaquille@toru.digital>
2025-11-29 18:08:01 +0000

    integrate tailwindcss

--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -18,7 +18,9 @@ function WelcomePage() {
           <h1>Welcome to Event Highway!</h1>
         </header>
         <section className="welcomeLayerTwo">
-          <button onClick={handleLoginClick}>Login</button>
+          <button onClick={handleLoginClick} className="!bg-red-300">
+            Login
+          </button>
           <button onClick={handleSignupClick}>Sign Up!</button>
         </section>
         <section className="welcomeLayerThree">
