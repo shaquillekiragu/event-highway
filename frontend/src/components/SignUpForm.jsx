548efeb5174372835e60c3e75222c3b218b36bbe
Shaquille Kiragu <shaquille@toru.digital>
2025-11-30 17:13:03 +0000

    replace vanilla css with tailwind 2

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -10,90 +10,113 @@ function SignUpForm({
   handleIsAdminChange,
 }) {
   return (
-    <form onSubmit={handleSubmit} className="signupForm">
-      <section className="formGroup">
-        <label htmlFor="email">Email Address:</label>
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-100 flex flex-col gap-4 my-0 mx-auto *:flex *:flex-col"
+    >
+      <section>
+        <label htmlFor="email" className="mb-1 font-bold">
+          Email Address:
+        </label>
         <input
           id="email"
           type="email"
           placeholder="Enter your email address"
           onChange={handleEmailChange}
+          className="p-2 border rounded-lg"
           required
         />
       </section>
 
-      <section className="formGroup">
-        <label htmlFor="password">Password:</label>
+      <section>
+        <label htmlFor="password" className="mb-1 font-bold">
+          Password:
+        </label>
         <input
           id="password"
           type="password"
           placeholder="Enter your password"
           onChange={handlePasswordChange}
+          className="p-2 border rounded-lg"
           required
         />
       </section>
 
-      <section className="formGroup">
-        <label htmlFor="first_name">First Name:</label>
+      <section>
+        <label htmlFor="first_name" className="mb-1 font-bold">
+          First Name:
+        </label>
         <input
           id="first_name"
           type="text"
           placeholder="Enter your first name"
           onChange={handleFirstNameChange}
+          className="p-2 border rounded-lg"
           required
         />
       </section>
 
-      <section className="formGroup">
-        <label htmlFor="last_name">Last Name:</label>
+      <section>
+        <label htmlFor="last_name" className="mb-1 font-bold">
+          Last Name:
+        </label>
         <input
           id="last_name"
           type="text"
           placeholder="Enter your last name (optional)"
           onChange={handleLastNameChange}
+          className="p-2 border rounded-lg"
         />
       </section>
 
-      <section className="formGroup">
-        <label htmlFor="display_name">Display Name:</label>
+      <section>
+        <label htmlFor="display_name" className="mb-1 font-bold">
+          Display Name:
+        </label>
         <input
           id="display_name"
           type="text"
           placeholder="Choose a display name"
           onChange={handleDisplayNameChange}
+          className="p-2 border rounded-lg"
           required
         />
       </section>
 
-      <fieldset className="formGroup">
-        <legend>Admin Status:</legend>
-        <section className="radioGroup">
+      <fieldset className="p-2 border rounded-lg">
+        <legend className="mb-1 font-bold">Admin Status:</legend>
+
+        <section className="flex items-center gap-2">
           <input
             id="admin_yes"
             type="radio"
             name="is_admin"
             value={true}
             onChange={handleIsAdminChange}
+            className="p-2 border rounded-lg"
           />
-          <label htmlFor="admin_yes">Yes</label>
+          <label htmlFor="admin_yes" className="mb-1 font-bold">
+            Yes
+          </label>
         </section>
-        <section className="radioGroup">
+
+        <section className="flex items-center gap-2">
           <input
             id="admin_no"
             type="radio"
             name="is_admin"
             value={false}
             onChange={handleIsAdminChange}
+            className="p-2 border rounded-lg"
             defaultChecked
           />
-          <label htmlFor="admin_no">No</label>
+          <label htmlFor="admin_no" className="mb-1 font-bold">
+            No
+          </label>
         </section>
       </fieldset>
 
-      <button
-        type="submit"
-        className="submitButton w-1/2 self-center ml-[40%] mt-7"
-      >
+      <button type="submit" className="w-1/2 self-center ml-[40%] mt-7 p-3">
         Sign Up
       </button>
     </form>
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/UserContext";
 import { postUser } from "../api";
-import SignUpForm from "../components/SignUpForm/SignUpForm.jsx";
+import SignUpForm from "../components/SignUpForm.jsx";
 import "../stylesheets/SignUpPage.css";
 
 function SignUpPage() {
